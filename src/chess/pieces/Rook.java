de1e66cca0521aebb1a99cb4fff8bd0970333bc5
Kaique Diniz <kaiquediniz1204@gmail.com>
2023-06-19 17:24:59 -0300

    Imprimindo os movimentos possíveis

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -54,7 +54,7 @@ public class Rook extends ChessPiece {
 		}
 		
 		// below
-		p.setValues(position.getRow() +1 , position.getColumn());
+		p.setValues(position.getRow() + 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setRow(p.getRow() + 1);
